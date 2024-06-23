bea14511888411d027424b27f49e61232e6f6d99
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-24 01:14:12 +0530

    CI CD deployment setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Response } from "express";
+
+const app = express();
+
+app.get("/", (req, res: Response) => {
+    res.send({
+        message: "Hello World",
+    });
+});
+
+app.listen(3000, () => {
+    console.log("App is listening on port 3000");
+});
